ca5c82948c502d45460d21243671393a3681335d
Ekta <ekta16567@gmail.com>
2023-08-23 17:11:57 +0530

    Add files via upload

---
+++ Project_Spotify/script.js
@@ -0,0 +1,106 @@
+console.log("Welcome to Spotify");
+//initializing variables
+let songIndex = 0;
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let songItem = Array.from(document.getElementsByClassName('songItem'));
+let masterSongName = document.getElementById('masterSongName');
+let audioElement = new Audio('songs/1.mp3');//add song url in ()
+//audioElement.play(2
+
+let songs = [
+    {songName: "Haryana Aale", filePath: "songs/1.mp3" , coverPath: "song_cover/1.jpg"},
+    {songName: "Salam-e-ishq", filePath: "songs/2.mp3" , coverPath: "covers/1.jpg"},
+    {songName: "Sanam Teri kasam", filePath: "songs/3.mp3" , coverPath: "covers/2.jpg"},
+    {songName: "Hua aaj Phli Bar", filePath: "songs/4.mp3" , coverPath: "covers/3.jpg"},
+    {songName: "Let me love you", filePath: "songs/5.mp3" , coverPath: "covers/4.jpg"},
+    {songName: "Let me down", filePath: "songs/6.mp3" , coverPath: "covers/5.jpg"},
+//filepath m agar folder ke ander mp3 h to likho - fileName/songName.mp3
+]       // same with cover path
+
+songItem.forEach((element, i)=>{ 
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+})
+ 
+
+//handle play/pause click
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+//Listen to event
+audioElement.addEventListener('timeupdate', ()=>{ 
+    // Update Seekbar
+     progress = parseInt((audioElement.currentTime/audioElement.duration)* 100);
+     myProgressBar.value = progress;
+})
+myProgressBar.addEventListener('change',()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration / 100;
+})
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songlistplay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+
+Array.from(document.getElementsByClassName('songlistplay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{ 
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=5){
+        songIndex = 0
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
+
